fdcabc9d606aa603300286724602f1c774075eab
debil228obosris <91481839+debil228obosrish@users.noreply.github.com>
2021-12-06 16:38:33 +0000

    dsdssd

---
+++ p6-t4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def circle (radius=10):
+  x= np.arange(-2*radius,2*radius,0.1)
+
+  y =np.arange(-2*radius,2*radius,0.1)
+  X,Y = np.meshgrid(x,y)
+  fxy = X**2 + Y**2
+  plt.contour (X, Y, fxy, levels=[radius**2] )
+  plt.show()
+
+def circle2 (radius=10):
+  x= np.arange(-20*radius,20*radius,0.1)
+  y =np.arange(-20*radius,20*radius,0.1)
+  a = 20
+  b = 10
+  X,Y = np.meshgrid(x,y)
+  fxy = X**2/a**2 + Y**2/b**2
+  plt.contour (X, Y, fxy, levels=[radius**2] )
+  plt.axis('equal')
+  plt.show()
+n = int(input())
+if (n==1):
+  circle()
+elif (n==2):
+  circle2()
---
+++ p6-t4d.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from math  import sqrt
+x = [5]
+y = [3]
+l = 10
+h = 1
+lastx = x
+lasty = y
+n = int(input())
+for i in range(n)
