a08746180cf72a121004fb7822e4e2a845882276
debil228obosris <91481839+debil228obosrish@users.noreply.github.com>
2021-12-27 15:19:24 +0000

    проект

---
+++ proekt.py
@@ -0,0 +1,22 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+def circle (radius=10):
+  x= np.arange(-2*radius,2*radius,0.1)
+
+  y =np.arange(-2*radius,2*radius,0.1)
+  X,Y = np.meshgrid(x,y)
+  fxy = X**2 + Y**2
+  plt.contour (X, Y, fxy, levels=[radius**2] )
+  plt.axis('equal')
+  plt.show()
+circle() 
+  
