925cbb2481778ace71ba615f274a53d4d9bcb86a
debil228obosris <91481839+debil228obosrish@users.noreply.github.com>
2022-01-16 20:11:15 +0000

    ffdfggd

--- proekt.py
+++ proekt.py
@@ -4,11 +4,6 @@ import numpy as np
 
 
 
-
-
-
-
-
 def circle (radius=10):
   x= np.arange(-2*radius,2*radius,0.1)
 
@@ -17,6 +12,16 @@ def circle (radius=10):
   fxy = X**2 + Y**2
   plt.contour (X, Y, fxy, levels=[radius**2] )
   plt.axis('equal')
+
+  def parabola_plotter(a=0.5, b=0.5, c=0, title='parabola  plotter'):
+    x = np.arange(-10,10,0.0001)
+    y = a*x**2 + b*x + c
+
+    plt.plot(x, y, label='my parabola', marker='>', ms=0.1)
+    plt.title(title)
+  parabola_plotter()
+
   plt.show()
-circle() 
-  
+circle()
+
+
